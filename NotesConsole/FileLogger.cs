7574ac320af99fcb7226d04f863297edf4c3c514
Michael Bui <bui.michael652@gmail.com>
2017-01-03 16:21:24 -0500

    wow
    wow

---
+++ NotesConsole/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NotesConsole
+{
+    public class FileLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ NotesConsole/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace NotesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"%userprofile%\desktop";
+
+            while (true)
+            {
+                Options.Menu();
+                var selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "1":
+                        Console.Write("Caller's Username: ");
+                        var input = Console.ReadLine();
+                        Console.Write("Notes: ");
+                        var notes = Console.ReadLine();
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+        
+    }
+
+    public class Notes
+    {
+        public Notes()
+        {
+            
+        }
+    }
+
+    public class NoteFormat
+    {
+        
+    }
+
+    public class FileWriter
+    {
+        public FileWriter(string path,string writtenInfo)
+        {
+            using (var streamWriter = new StreamWriter(path))
+            {
+                if (File.Exists(path + "Call_Notes.txt"))
+
+                streamWriter.Write(writtenInfo);
+            }
+        }
+    }
+}
